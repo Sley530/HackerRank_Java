2225890bed8eb5d4b3e83dccad7c0e44d5630101
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-02-09 01:48:43 -0500

    Update

---
+++ Std_io_2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Std_io_2 {
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        double num = scan.nextDouble();
+        int integer = scan.nextInt();
+        scan.close();
+
+        System.out.println("String: " + str);
+        System.out.println("Double: " + num);
+        System.out.println("Int: " + integer);
+
+    }
+
+}
