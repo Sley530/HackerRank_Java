540253c783ce37c43d080de583cee15288107083
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-02-09 02:42:18 -0500

    Update

--- Std_io_2.java
+++ Std_io_2.java
@@ -1,20 +1,21 @@
 import java.io.*;
 import java.util.*;
 
-public class Std_io_2 {
+public class Std_io {
 
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
 
-        String str = scan.next();
-        double num = scan.nextDouble();
-        int integer = scan.nextInt();
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String str = scan.nextLine();
+        String s = scan.nextLine();
         scan.close();
 
-        System.out.println("String: " + str);
-        System.out.println("Double: " + num);
-        System.out.println("Int: " + integer);
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
 
     }
 
