b25852e9e4503f538c4b7efda2bf5b6de72a66df
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-02-11 22:31:16 -0500

    Update

--- IO_format.java
+++ IO_format.java
@@ -5,7 +5,7 @@ public class IO_format {
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             System.out.println("================================");
-            for(int i=0;i<3;i++)
+            for(int i = 0 ;i<3;i++)
             {
                 String s1=sc.next();
                 int x=sc.nextInt();
